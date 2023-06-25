05bc62366fbd9addff8ebfbb57b8af5c4a696b9b
EKATERINA_ZAKHAROVA <93439877+Flowruns@users.noreply.github.com>
2023-06-25 20:41:43 +0400

    Delete Node_JS/Node_JS directory

--- Node_JS/Node_JS/1-3_lesson/1-3_lesson.js
+++
@@ -1,9 +0,0 @@
-// подключаем модуль (библиотеку) "Конвертер валют"
-const CC = require('currency-converter-lt')
-// создаем объект, на основе подключенного класса, тут же
-// указываем валюту
-let currencyConverter = new CC({from:"USD", to:"JPY", amount:100})
-// обращаемся к самой функции конвертирования, далее выполняем функцую, принимающую как параметр ответ
-currencyConverter.convert().then((response) => {
-    console.log(response) 
-})
